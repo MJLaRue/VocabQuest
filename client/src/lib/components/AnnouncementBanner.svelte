9a69316ba13412b0bbe8aa0e714403800d7afcc0
Michael LaRue <mlarue4@uic.edu>
2026-04-26 13:44:50 -0500

    feat: add AnnouncementBanner component

---
+++ client/src/lib/components/AnnouncementBanner.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  export let text: string = '';
+</script>
+
+{#if text}
+  <div
+    role="banner"
+    class="bg-teal-600 text-white text-sm text-center py-2 px-4 font-medium"
+  >
+    {text}
+  </div>
+{/if}
